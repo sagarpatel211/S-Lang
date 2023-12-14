7f313592b9805bd46961be3435695a6bddf78324
Sagar Patel <2sagarpatel2@gmail.com>
2023-12-14 04:14:01 -0500

    Finally completed Parser step implementation!

--- include/ast.hpp
+++ include/ast.hpp
@@ -2,6 +2,299 @@
 #define AST_HPP
 #pragma once
 
-// TO DO: Add AST Code
+#include "debug_stream.hpp"
+#include <memory>
+#include <optional>
+#include <set>
+#include <string>
+#include <variant>
+#include <vector>
+
+template <typename T>
+class Literal; // Done
+class VariableExpression;
+class UnaryExpression;
+class BinaryExpression;
+class CallExpression;
+class CookedUpStatement; // Done
+class AssignmentStatement; // Done
+class CookedUpAssignmentStatement; // Done
+class FrOngJustLikeThatStatement; // Done
+class holdUpStatement; // Done
+class RatioedStatement; // Done
+class CompoundStatement;  // Done
+class PeriodtStatement; // Done
+class RizzStatement; // Done
+class GhostStatement; // Done
+class GangStatement; // Done
+class PluhDeclaration;
+class Prototype;
+class TeaSpill;
+
+template <typename T>
+inline bool operator==(const std::unique_ptr<T>& p1, const std::unique_ptr<T>& p2) {
+    if (p1 != nullptr && p2 != nullptr) {
+        return *p1 == *p2;
+    }
+    return p1 == p2;
+}
+
+template <typename T>
+class Literal {
+  private:
+    T value;
+  public:
+    Literal(Literal<T>&&) = default;
+    explicit Literal(T value);
+    T get_value() const;
+    Literal<T>& operator=(Literal<T>&&) = default;
+    bool operator==(const Literal<T>& rhs) const = default;
+};
+
+using Expression =
+    std::variant<Literal<int>, Literal<double>, Literal<bool>, Literal<char>,
+                 Literal<std::string>, std::unique_ptr<VariableExpression>, std::unique_ptr<UnaryExpression>,
+                 std::unique_ptr<BinaryExpression>, std::unique_ptr<CallExpression>>;
+
+class VariableExpression {
+  private:
+    std::string name;
+  public:
+    VariableExpression(VariableExpression&&) = default;
+    explicit VariableExpression(const std::string& name);
+    std::string get_name() const;
+    VariableExpression& operator=(VariableExpression&&) = default;
+    bool operator==(const VariableExpression& rhs) const = default;
+};
+
+class UnaryExpression {
+  private:
+    std::string op;
+    Expression rhs;
+  public:
+    UnaryExpression(UnaryExpression&&) = default;
+    explicit UnaryExpression(const std::string& op, Expression rhs);
+    std::string get_op() const;
+    Expression& get_rhs();
+    UnaryExpression& operator=(UnaryExpression&&) = default;
+    bool operator==(const UnaryExpression& rhs) const = default;
+};
+
+class BinaryExpression {
+  private:
+    std::string op;
+    Expression lhs;
+    Expression rhs;
+  public:
+    BinaryExpression(BinaryExpression&&) = default;
+    explicit BinaryExpression(const std::string& op, Expression lhs, Expression rhs);
+    std::string get_op() const;
+    Expression& get_lhs();
+    Expression& get_rhs();
+    BinaryExpression& operator=(BinaryExpression&&) = default;
+    bool operator==(const BinaryExpression& rhs) const = default;
+};
+
+class CallExpression {
+  private:
+    std::string callee;
+    std::vector<Expression> arguments;
+  public:
+    CallExpression(CallExpression&&) = default;
+    explicit CallExpression(const std::string& callee, std::vector<Expression> arguments);
+    std::string get_callee() const;
+    std::vector<Expression>& get_arguments();
+    CallExpression& operator=(CallExpression&&) = default;
+    bool operator==(const CallExpression& rhs) const = default;
+};
+
+using Argument = std::pair<std::string, std::string>;
+
+class Prototype {
+  private:
+    std::string name;
+    std::vector<Argument> arguments;
+    std::string return_type;
+  public:
+    Prototype(Prototype&&) = default;
+    explicit Prototype(const std::string& name, std::vector<Argument> arguments, const std::string& return_type);
+    Prototype& operator=(Prototype&&) = default;
+    std::string get_name() const;
+    std::vector<Argument>& get_arguments();
+    std::string get_return_type() const;
+    bool operator==(const Prototype& rhs) const = default;
+};
+
+class CookedUpStatement {
+  private:
+    std::string var_name;
+    std::string var_type;
+  public:
+    CookedUpStatement(CookedUpStatement&&) = default;
+    explicit CookedUpStatement(const std::string& var_name, const std::string& var_type);
+    std::string get_var_name() const;
+    std::string get_var_type() const;
+    CookedUpStatement& operator=(CookedUpStatement&&) = default;
+    bool operator==(const CookedUpStatement& rhs) const = default;
+};
+
+class AssignmentStatement {
+  private:
+    std::string var_name;
+    Expression assignment_expression;
+  public:
+    AssignmentStatement(AssignmentStatement&&) = default;
+    explicit AssignmentStatement(const std::string& var_name, Expression assignment_expression);
+    std::string get_var_name() const;
+    Expression& get_assignment_expression();
+    AssignmentStatement& operator=(AssignmentStatement&&) = default;
+    bool operator==(const AssignmentStatement& rhs) const = default;
+};
+
+class CookedUpAssignmentStatement {
+  private:
+    std::string var_name;
+    std::string var_type;
+    Expression assignment_expression;
+  public:
+    CookedUpAssignmentStatement(CookedUpAssignmentStatement&&) = default;
+    explicit CookedUpAssignmentStatement(const std::string& var_name, const std::string& var_type,
+                                    Expression assignment_expression);
+    std::string get_var_name() const;
+    std::string get_var_type() const;
+    Expression& get_assignment_expression();
+    CookedUpAssignmentStatement& operator=(CookedUpAssignmentStatement&&) = default;
+    bool operator==(const CookedUpAssignmentStatement& rhs) const = default;
+};
+
+class PeriodtStatement {
+  private:
+    Expression return_expr;
+  public:
+    PeriodtStatement(PeriodtStatement&&) = default;
+    explicit PeriodtStatement(Expression return_expr);
+    Expression& get_return_expr();
+    PeriodtStatement& operator=(PeriodtStatement&&) = default;
+    bool operator==(const PeriodtStatement& rhs) const = default;
+};
+
+class GhostStatement {
+  public:
+    GhostStatement(GhostStatement&&) = default;
+    explicit GhostStatement() = default;
+    GhostStatement& operator=(GhostStatement&&) = default;
+    bool operator==(const GhostStatement& rhs) const = default;
+};
+
+class RizzStatement {
+  public:
+    RizzStatement(RizzStatement&&) = default;
+    explicit RizzStatement() = default;
+    RizzStatement& operator=(RizzStatement&&) = default;
+    bool operator==(const RizzStatement& rhs) const = default;
+};
+
+class GangStatement {
+  private:
+    std::string name;
+    std::string type;
+    std::vector<Expression> values;
+  public:
+    GangStatement(GangStatement&&) = default;
+    explicit GangStatement(const std::string& name, const std::string& type, std::vector<Expression> values);
+    std::string get_name() const;
+    std::string get_type() const;
+    std::vector<Expression>& get_values();
+    GangStatement& operator=(GangStatement&&) = default;
+    bool operator==(const GangStatement& rhs) const = default;
+};
+
+using Statement =
+    std::variant<std::unique_ptr<CookedUpStatement>, std::unique_ptr<AssignmentStatement>,
+                 std::unique_ptr<CookedUpAssignmentStatement>, std::unique_ptr<FrOngJustLikeThatStatement>, std::unique_ptr<holdUpStatement>,
+                 std::unique_ptr<GhostStatement>, std::unique_ptr<RizzStatement>, std::unique_ptr<PeriodtStatement>,
+                 std::unique_ptr<CompoundStatement>, std::unique_ptr<RatioedStatement>, std::unique_ptr<GangStatement>>;
+
+class FrOngJustLikeThatStatement {
+  private:
+    Expression condition;
+    Statement then_statement;
+    Statement else_statement;
+  public:
+    FrOngJustLikeThatStatement(FrOngJustLikeThatStatement&&) = default;
+    explicit FrOngJustLikeThatStatement(Expression condition, Statement then_statement, Statement else_statement);
+    Expression& get_condition();
+    Statement& get_then_statement();
+    Statement& get_else_statement();
+    FrOngJustLikeThatStatement& operator=(FrOngJustLikeThatStatement&&) = default;
+    bool operator==(const FrOngJustLikeThatStatement& rhs) const = default;
+};
+
+class holdUpStatement {
+  private:
+    Expression condition;
+    Statement body;
+  public:
+    holdUpStatement(holdUpStatement&&) = default;
+    explicit holdUpStatement(Expression condition, Statement body);
+    Expression& get_condition();
+    Statement& get_body();
+    holdUpStatement& operator=(holdUpStatement&&) = default;
+    bool operator==(const holdUpStatement& rhs) const = default;
+};
+
+class RatioedStatement {
+  private:
+    Statement init;
+    Expression condition;
+    Statement increment;
+    Statement body;
+  public:
+    RatioedStatement(RatioedStatement&&) = default;
+    explicit RatioedStatement(Statement init, Expression condition, Statement increment, Statement body);
+    Statement& get_init();
+    Expression& get_condition();
+    Statement& get_increment();
+    Statement& get_body();
+    RatioedStatement& operator=(RatioedStatement&&) = default;
+    bool operator==(const RatioedStatement& rhs) const = default;
+};
+
+class CompoundStatement {
+  private:
+    std::vector<Statement> statements;
+  public:
+    CompoundStatement(CompoundStatement&&) = default;
+    explicit CompoundStatement(std::vector<Statement> statements);
+    std::vector<Statement>& get_statements();
+    CompoundStatement& operator=(CompoundStatement&&) = default;
+    bool operator==(const CompoundStatement& rhs) const = default;
+};
+
+class PluhDeclaration {
+  private:
+    Prototype p;
+    std::optional<Statement> function_body;
+  public:
+    PluhDeclaration(PluhDeclaration&&) = default;
+    explicit PluhDeclaration(Prototype p, Statement function_body);
+    explicit PluhDeclaration(Prototype p);
+    Prototype& get_prototype();
+    std::optional<Statement>& get_function_body();
+    PluhDeclaration& operator=(PluhDeclaration&&) = default;
+    bool operator==(const PluhDeclaration& rhs) const = default;
+};
+
+class TeaSpill {
+  private:
+    std::string name;
+    std::vector<std::variant<PluhDeclaration>> declarations;
+  public:
+    TeaSpill(TeaSpill&&) = default;
+    std::string get_name() const;
+    std::vector<std::variant<PluhDeclaration>>& get_declarations();
+    explicit TeaSpill(const std::string& name, std::vector<std::variant<PluhDeclaration>> declarations);
+    TeaSpill& operator=(TeaSpill&&) = default;
+};
 
 #endif
--- src/ast.cpp
+++ src/ast.cpp
@@ -1,3 +1,246 @@
 #include "ast.hpp"
 
-// TO DO
+template <typename T>
+Literal<T>::Literal(T value) : value(value) {}
+
+template <typename T>
+T Literal<T>::get_value() const {
+    return value;
+}
+
+VariableExpression::VariableExpression(const std::string& name) : name(name) {
+    debug << "[DEBUG] Variable Expression Initialized: " << name << std::endl;
+}
+
+std::string VariableExpression::get_name() const {
+	return name;
+}
+
+UnaryExpression::UnaryExpression(const std::string& op, Expression rhs) : op(op), rhs(std::move(rhs)) {
+    debug << "[DEBUG] Unary Expression Initialized: " << op << std::endl;
+}
+
+std::string UnaryExpression::get_op() const {
+	return op;
+}
+
+Expression &UnaryExpression::get_rhs() {
+	return rhs;
+}
+
+BinaryExpression::BinaryExpression(const std::string& op, Expression lhs, Expression rhs)
+    : op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {
+    debug << "[DEBUG] Binary Expression Initialized: " << op << std::endl;
+}
+
+std::string BinaryExpression::get_op() const {
+	return op;
+}
+
+Expression &BinaryExpression::get_lhs() {
+	return lhs;
+}
+
+Expression &BinaryExpression::get_rhs() {
+	return rhs;
+}
+
+CallExpression::CallExpression(const std::string& callee, std::vector<Expression> arguments)
+    : callee(callee), arguments(std::move(arguments)) {
+    debug << "[DEBUG] Call Expression Initialized: " << callee << std::endl;
+}
+
+std::string CallExpression::get_callee() const {
+	return callee;
+}
+
+std::vector<Expression> &CallExpression::get_arguments() {
+	return arguments;
+}
+
+Prototype::Prototype(const std::string& name, std::vector<std::pair<std::string, std::string>> arguments,
+                     const std::string& return_type)
+    : name(name), arguments(arguments), return_type(return_type) {
+    debug << "[DEBUG] Prototype Initialized: " << name << std::endl;
+}
+
+std::string Prototype::get_name() const {
+    return name;
+}
+
+std::vector<Argument> &Prototype::get_arguments() {
+	return arguments;
+}
+
+std::string Prototype::get_return_type() const {
+	return return_type;
+}
+
+CookedUpStatement::CookedUpStatement(const std::string& var_name, const std::string& var_type)
+    : var_name(var_name), var_type(var_type) {
+    debug << "[DEBUG] cookUp Statement Initialized: " << var_name << std::endl;
+}
+
+std::string CookedUpStatement::get_var_name() const {
+	return var_name;
+}
+
+std::string CookedUpStatement::get_var_type() const {
+	return var_type;
+}
+
+AssignmentStatement::AssignmentStatement(const std::string& var_name, Expression assignment_expression)
+    : var_name(var_name), assignment_expression(std::move(assignment_expression)) {
+    debug << "[DEBUG] Assignment Statement Initialized: " << var_name << std::endl;
+}
+
+std::string AssignmentStatement::get_var_name() const {
+	return var_name;
+}
+
+Expression &AssignmentStatement::get_assignment_expression() {
+	return assignment_expression;
+}
+
+CookedUpAssignmentStatement::CookedUpAssignmentStatement(const std::string& var_name, const std::string& var_type,
+                                               Expression assignment_expression)
+    : var_name(var_name), var_type(var_type), assignment_expression(std::move(assignment_expression)) {
+    debug << "[DEBUG] cookUp AssignmentStatement Initialized: " << var_name << std::endl;
+}
+
+std::string CookedUpAssignmentStatement::get_var_name() const {
+    return var_name;
+}
+
+std::string CookedUpAssignmentStatement::get_var_type() const {
+    return var_type;
+}
+
+Expression &CookedUpAssignmentStatement::get_assignment_expression() {
+    return assignment_expression;
+}
+
+PeriodtStatement::PeriodtStatement(Expression return_expr) : return_expr(std::move(return_expr)) {
+    debug << "[DEBUG] Periodt Statement Initialized" << std::endl;
+}
+
+Expression &PeriodtStatement::get_return_expr() {
+	return return_expr;
+}
+
+GangStatement::GangStatement(const std::string& name, const std::string& type, std::vector<Expression> values)
+    : name(name), type(type), values(std::move(values)) {
+    debug << "[DEBUG] Gang Statement Initialized: " << name << std::endl;
+}
+
+std::string GangStatement::get_name() const {
+    return name;
+}
+
+std::string GangStatement::get_type() const {
+    return type;
+}
+
+std::vector<Expression> &GangStatement::get_values() {
+    return values;
+}
+
+FrOngJustLikeThatStatement::FrOngJustLikeThatStatement(Expression condition, Statement then_statement, Statement else_statement)
+    : condition(std::move(condition)),
+      then_statement(std::move(then_statement)),
+      else_statement(std::move(else_statement)) {
+    debug << "[DEBUG] Fr?Ong?justLikeThat? Statement Initialized" << std::endl;
+}
+
+Expression &FrOngJustLikeThatStatement::get_condition() {
+	return condition;
+}
+
+Statement &FrOngJustLikeThatStatement::get_then_statement() {
+	return then_statement;
+}
+
+Statement &FrOngJustLikeThatStatement::get_else_statement() {
+	return else_statement;
+}
+
+holdUpStatement::holdUpStatement(Expression condition, Statement body)
+    : condition(std::move(condition)), body(std::move(body)) {
+    debug << "[DEBUG] holdUp Statement Initialized" << std::endl;
+}
+
+Expression &holdUpStatement::get_condition() {
+	return condition;
+}
+
+Statement &holdUpStatement::get_body() {
+	return body;
+}
+
+RatioedStatement::RatioedStatement(Statement init, Expression condition, Statement increment, Statement body)
+    : init(std::move(init)),
+      condition(std::move(condition)),
+      increment(std::move(increment)),
+      body(std::move(body)) {
+    debug << "[DEBUG] Ratioed Statement Initialized" << std::endl;
+}
+
+Statement &RatioedStatement::get_init() {
+    return init;
+}
+
+Expression &RatioedStatement::get_condition() {
+    return condition;
+}
+
+Statement &RatioedStatement::get_increment() {
+    return increment;
+}
+
+Statement &RatioedStatement::get_body() {
+    return body;
+}
+
+CompoundStatement::CompoundStatement(std::vector<Statement> statements) : statements(std::move(statements)) {
+    debug << "[DEBUG] Compound Statement Initialized" << std::endl;
+}
+
+std::vector<Statement> &CompoundStatement::get_statements() {
+	return statements;
+}
+
+PluhDeclaration::PluhDeclaration(Prototype p, Statement function_body)
+    : p(std::move(p)), function_body(std::move(function_body)) {
+    debug << "[DEBUG] Pluh Declaration Initialized: " << p.get_name() << std::endl;
+}
+
+PluhDeclaration::PluhDeclaration(Prototype proto) : p(std::move(p)), function_body({}) {
+    debug << "[DEBUG] Pluh Declaration Initialized: " << p.get_name() << std::endl;
+}
+
+Prototype &PluhDeclaration::get_prototype() {
+	return p;
+}
+
+std::optional<Statement> &PluhDeclaration::get_function_body() {
+	return function_body;
+}
+
+TeaSpill::TeaSpill(const std::string& name, std::vector<std::variant<PluhDeclaration>> declarations)
+    : name(name), declarations(std::move(declarations)) {
+    debug << "[DEBUG] SpillingTheTeaAbout: " << name << std::endl;
+}
+
+std::string TeaSpill::get_name() const {
+	return name;
+}
+
+std::vector<std::variant<PluhDeclaration>>& TeaSpill::get_declarations() {
+	return declarations;
+}
+
+template class Literal<int>;
+template class Literal<double>;
+template class Literal<bool>;
+template class Literal<char>;
+template class Literal<std::string>;
